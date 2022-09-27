f262f9efea8f5dec42575023f51dd045b6d9885d
Harun Nurahman <harun.nurrahman@hotmail.com>
2022-09-27 11:11:51 +0700

    Project API Configuration

---
+++ lib/services/api_services.dart
@@ -0,0 +1,6 @@
+class ApiServices {
+  static const baseUrl = 'http://192.168.0.103/api_money-record';
+
+  static const historyUrl = '$baseUrl/history/';
+  static const userUrl = '$baseUrl/user/';
+}
