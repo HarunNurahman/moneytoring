3b3aeca05e5482ea6b194b06fd7ed35970209fa4
Harun Nurahman <harun.nurrahman@hotmail.com>
2022-10-12 20:40:11 +0700

    Slicing UI Add Transaction Page

--- lib/pages/add-history_page.dart
+++ lib/pages/add-history_page.dart
@@ -46,13 +46,11 @@ class AddHistoryPage extends StatelessWidget {
       );
     }
 
-    return Scaffold(
-      appBar: DView.appBarLeft('Tambah Transaksi Baru'),
-      body: ListView(
-        padding: const EdgeInsets.all(16),
+    // Body (tipe transaksi, objek pemasukan/pengeluaran, nilai transaksi, daftar transaksi, total transaksi)
+    Widget body() {
+      return Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          header(),
-          DView.spaceHeight(),
           // Transaction type
           Text('Tipe', style: blackTextStyle.copyWith(fontWeight: semiBold)),
           DView.spaceHeight(8),
@@ -108,6 +106,89 @@ class AddHistoryPage extends StatelessWidget {
               ),
             ),
           ),
+          DView.spaceHeight(24),
+          // Transaction items
+          Text(
+            'Daftar Transaksi',
+            style: blackTextStyle.copyWith(fontWeight: semiBold),
+          ),
+          DView.spaceHeight(8),
+          Container(
+            width: double.infinity,
+            padding: const EdgeInsets.all(16),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(8),
+              border: Border.all(color: Colors.grey),
+            ),
+            child: Chip(
+              label: Text(
+                'Bensin',
+                style: blackTextStyle.copyWith(
+                  fontSize: 12,
+                ),
+              ),
+              deleteIcon: const Icon(Icons.clear_rounded),
+              onDeleted: () {},
+            ),
+          ),
+          DView.spaceHeight(24),
+          // Total transaction
+          Row(
+            children: [
+              RichText(
+                text: TextSpan(
+                  text: 'Total Transaksi: \n',
+                  style: blackTextStyle,
+                  children: [
+                    TextSpan(
+                      text: AppFormat.currencyFormat('300000'),
+                      style: blueTextStyle.copyWith(
+                        fontSize: 24,
+                        fontWeight: semiBold,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ],
+      );
+    }
+
+    // Add transaction button
+    Widget submitTransaction() {
+      return Container(
+        width: double.infinity,
+        height: 50,
+        child: ElevatedButton(
+          onPressed: () {},
+          style: ElevatedButton.styleFrom(
+              primary: kPrimaryColor,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+              )),
+          child: Text(
+            'Tambah Transaksi',
+            style: whiteTextStyle.copyWith(
+              fontSize: 16,
+              fontWeight: semiBold,
+            ),
+          ),
+        ),
+      );
+    }
+
+    return Scaffold(
+      appBar: DView.appBarLeft('Tambah Transaksi Baru'),
+      body: ListView(
+        padding: const EdgeInsets.all(16),
+        children: [
+          header(),
+          DView.spaceHeight(),
+          body(),
+          DView.spaceHeight(24),
+          submitTransaction(),
         ],
       ),
     );
